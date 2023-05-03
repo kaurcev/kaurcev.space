cc65ae494b03dd9c108a15dfe7c5f209d66bf456
kaurcev <skaurcev2004@yandex.ru>
2023-05-04 01:13:25 +0600

    update

---
+++ assets/code.js
@@ -0,0 +1,4 @@
+console.clear();
+console.log('%cСТОП СТОП СТОП!!!', 'color: red; background: #212529; font-size: 32px');
+console.log('%cТут тебе явно делать нечего', 'color: yellow; background: #212529; font-size: 16px');
+console.log('%cДумаю, что после старта работы сайта, тебе стоит посетить эту страницу -> https://kaurcev.space/jops', 'color: white; background: #212529; font-size: 12px');
